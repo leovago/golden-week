daa252be9a40bc4862505968f22cd4eff4178283
Glitch (golden-week) <none>
2019-12-12 14:36:47 +0000

    👑🍎 Services up and running, passing all test

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+console.clear();
 // server.js
 // where your node app starts
 
@@ -24,7 +25,33 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// developer endpoint
+app.get("/api/developer", function(req, res){
+  res.json({
+    "developer":"Leo Vargas",
+    "company":"Magno Technologies"
+  });
+});
 
+// Empty date string
+app.get("/api/timestamp/", function(req, res){
+  let newDate = new Date();
+  res.json(getJSON(newDate));
+});
+
+app.get("/api/timestamp/:date_string", function(req, res){
+  let dateString = req.params.date_string;
+  let date = new Date();
+  (isNaN(dateString)) ? date = new Date(dateString) : date = new Date(Number(dateString));
+  (date == 'Invalid Date') ? res.json({"error":"Invalid Date"}) : res.json(getJSON(date));
+});
+
+function getJSON(date){
+  return ({
+    "unix": date.valueOf(),
+    "utc": date.toUTCString()
+  });
+}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
